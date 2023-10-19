e6de503f685bf52de31afcbaa0ee05f9548c485e
Gabriel Saturi <gabrielsaturi11@gmail.com>
2023-10-19 13:11:21 -0300

    Initial commit

---
+++ ebulição.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	ebulição := 100
+	kelvin := (float64(ebulição) + 273)
+	fmt.Printf("o valor de ebulição da agua é %d, e convertido para kelvin é %f", ebulição, kelvin)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module dio-go
+
+go 1.21.1
